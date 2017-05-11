bdb14c41315e0d92e384dbf8db983dfc8bf2670f
TestHefan <wangyanxia@hefantv.com>
2017-05-11 11:49:56 +0800

    针对手机系统是6.0的，自动点击请求的权限

--- src/qudaohao/Qdhtest.java
+++ src/qudaohao/Qdhtest.java
@@ -27,7 +27,10 @@ import org.testng.annotations.Test;
 
 import com.sun.jna.platform.unix.X11.XClientMessageEvent.Data;
 
+import io.appium.java_client.android.AndroidDeviceActionShortcuts;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidMobileCommandHelper;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
 
 public class Qdhtest {
 	private static AndroidDriver driver;
@@ -38,49 +41,142 @@ public class Qdhtest {
 
 	//static String[] apks = { "app-_360ShouJiZhuShou-release.apk", "app-TencentYingYongBao-release.apk", };
 
-	static String[] apks = { "app-TencentYingYongBao-release.apk", "app-BaiDuShouJiZhuShou-release.apk",
-			"app-XiaoMiYingYongShangDian-release.apk", "app-_360ShouJiZhuShou-release.apk",
-			"app-HuaWeiYingYongShangDian-release.apk", "app-OppoYingYongShangDian-release.apk",
-			"app-VivoYingYongShangDian-release.apk", "app-LeshiYingYongShangDian-release.apk",
-			"app-PPZhuShou-release.apk", "app-LenovoLeShangDian-release.apk", "app-WanDouJia-release.apk",
-			"app-SamsungYingYongShangDian-release.apk", "app-MeiZuYingYongShangDian-release.apk",
-			"app-SouGouYingYongShangDian-release.apk", "app-AnZhiYingYongShiChang-release.apk",
-			"app-HFLiveGuanWang-release.apk", "app-HFsem1-release.apk", "app-HFsem2-release.apk",
-			"app-HFsem3-release.apk", "app-HFsem4-release.apk", "app-HFsem5-release.apk", "app-HFsem6-release.apk",
-			"app-A_SC_TouTiao-release.apk", "app-A_SC_ShenMa-release.apk", "app-hf_test1-release.apk",
-			"app-hf_test2-release.apk", "app-hf_test3-release.apk", "app-hf_test4-release.apk",
-			"app-hf_test5-release.apk", "app-hf_h5-release.apk", "app-weibo-release.apk", "app-sina-release.apk",
-			"app-youdao-release.apk", "app-hf_test6-release.apk", "app-hf_test7-release.apk",
-			"app-hf_test8-release.apk", "app-hf_test9-release.apk", "app-hf_test10-release.apk",
-			"app-hf_test11-release.apk", "app-aliyun-release.apk", "app-jinli-release.apk", "app-mumayi-release.apk",
-			"app-hf_test12-release.apk", "app-hf_test13-release.apk", "app-hf_test14-release.apk",
-			"app-hf_test15-release.apk", "app-hf_test16-release.apk", "app-hf_s1-release.apk", "app-hf_s2-release.apk",
-			"app-hf_s3-release.apk", "app-hf_s4-release.apk", "app-hf_s5-release.apk", "app-hf_s6-release.apk",
-			"app-hf_s7-release.apk", "app-hf_s8-release.apk", "app-hf_s9-release.apk", "app-hf_s10-release.apk",
-			"app-hf_s11-release.apk", "app-hf_s12-release.apk", "app-hf_s13-release.apk", "app-hf_s14-release.apk",
-			"app-hf_s15-release.apk", "app-hf_s16-release.apk", "app-hf_s17-release.apk", "app-hf_s18-release.apk",
-			"app-hf_s19-release.apk", "app-hf_s20-release.apk", "app-hf_s21-release.apk", "app-hf_s22-release.apk",
-			"app-hf_s23-release.apk", "app-hf_s24-release.apk", "app-hf_s25-release.apk", "app-hf_s26-release.apk",
-			"app-hf_s27-release.apk", "app-hf_s28-release.apk", "app-hf_s29-release.apk", "app-hf_s30-release.apk",
-			"app-hf_s31-release.apk", "app-hf_s32-release.apk", "app-hf_s33-release.apk", "app-hf_s34-release.apk",
-			"app-hf_s35-release.apk", "app-hf_s36-release.apk", "app-hf_s37-release.apk", "app-hf_s38-release.apk",
-			"app-hf_s39-release.apk", "app-hf_s40-release.apk", "app-hf_s41-release.apk", "app-hf_s42-release.apk",
-			"app-hf_s43-release.apk", "app-hf_s44-release.apk", "app-hf_s45-release.apk", "app-hf_s46-release.apk",
-			"app-hf_s47-release.apk", "app-hf_s48-release.apk", "app-hf_s49-release.apk", "app-hf_s50-release.apk",
-			"app-hf_s51-release.apk", "app-hf_s52-release.apk", "app-hf_s53-release.apk", "app-hf_s54-release.apk",
-			"app-hf_s55-release.apk", "app-hf_s56-release.apk", "app-hf_s57-release.apk", "app-hf_s58-release.apk",
-			"app-hf_s59-release.apk", "app-hf_s60-release.apk", "app-hf_s61-release.apk", "app-hf_s62-release.apk",
-			"app-hf_s63-release.apk", "app-hf_s64-release.apk", "app-hf_s65-release.apk", "app-hf_s66-release.apk",
-			"app-hf_s67-release.apk", "app-hf_s68-release.apk", "app-hf_s69-release.apk", "app-hf_s70-release.apk",
-			"app-hf_s71-release.apk", "app-hf_s72-release.apk", "app-hf_s73-release.apk", "app-hf_s74-release.apk",
-			"app-hf_s75-release.apk", "app-hf_s76-release.apk", "app-hf_s77-release.apk", "app-hf_s78-release.apk",
-			"app-hf_s79-release.apk", };
+	static String[] apks = {			
+			"app-TencentYingYongBao-release.apk",
+			"app-BaiDuShouJiZhuShou-release.apk",
+			"app-XiaoMiYingYongShangDian-release.apk",
+			"app-_360ShouJiZhuShou-release.apk",
+			"app-HuaWeiYingYongShangDian-release.apk",
+			"app-OppoYingYongShangDian-release.apk",
+			"app-VivoYingYongShangDian-release.apk",
+			"app-LeshiYingYongShangDian-release.apk",
+			"app-PPZhuShou-release.apk",
+			"app-LenovoLeShangDian-release.apk",
+			"app-WanDouJia-release.apk",
+			"app-SamsungYingYongShangDian-release.apk",
+			"app-MeiZuYingYongShangDian-release.apk",
+			"app-SouGouYingYongShangDian-release.apk",
+			"app-AnZhiYingYongShiChang-release.apk",
+			"app-HFLiveGuanWang-release.apk",
+			"app-HFsem1-release.apk",
+			"app-HFsem2-release.apk",
+			"app-HFsem3-release.apk",
+			"app-HFsem4-release.apk",
+			"app-HFsem5-release.apk",
+			"app-HFsem6-release.apk",
+			"app-A_SC_TouTiao-release.apk",
+			"app-A_SC_ShenMa-release.apk",
+			"app-hf_test1-release.apk",
+			"app-hf_test2-release.apk",
+			"app-hf_test3-release.apk",
+			"app-hf_test4-release.apk",
+			"app-hf_test5-release.apk",
+			"app-hf_h5-release.apk",
+			"app-weibo-release.apk",
+			"app-sina-release.apk",
+			"app-youdao-release.apk",
+			"app-hf_test6-release.apk",
+			"app-hf_test7-release.apk",
+			"app-hf_test8-release.apk",
+			"app-hf_test9-release.apk",
+			"app-hf_test10-release.apk",
+			"app-hf_test11-release.apk",
+			"app-aliyun-release.apk",
+			"app-jinli-release.apk",
+			"app-mumayi-release.apk",
+			"app-hf_test12-release.apk",
+			"app-hf_test13-release.apk",
+			"app-hf_test14-release.apk",
+			"app-hf_test15-release.apk",
+			"app-hf_test16-release.apk",
+			"app-hf_s1-release.apk",
+			"app-hf_s2-release.apk",
+			"app-hf_s3-release.apk",
+			"app-hf_s4-release.apk",
+			"app-hf_s5-release.apk",
+			"app-hf_s6-release.apk",
+			"app-hf_s7-release.apk",
+			"app-hf_s8-release.apk",
+			"app-hf_s9-release.apk",
+			"app-hf_s10-release.apk",
+			"app-hf_s11-release.apk",
+			"app-hf_s12-release.apk",
+			"app-hf_s13-release.apk",
+			"app-hf_s14-release.apk",
+			"app-hf_s15-release.apk",
+			"app-hf_s16-release.apk",
+			"app-hf_s17-release.apk",
+			"app-hf_s18-release.apk",
+			"app-hf_s19-release.apk",
+			"app-hf_s20-release.apk",
+			"app-hf_s21-release.apk",
+			"app-hf_s22-release.apk",
+			"app-hf_s23-release.apk",
+			"app-hf_s24-release.apk",
+			"app-hf_s25-release.apk",
+			"app-hf_s26-release.apk",
+			"app-hf_s27-release.apk",
+			"app-hf_s28-release.apk",
+			"app-hf_s29-release.apk",
+			"app-hf_s30-release.apk",
+			"app-hf_s31-release.apk",
+			"app-hf_s32-release.apk",
+			"app-hf_s33-release.apk",
+			"app-hf_s34-release.apk",
+			"app-hf_s35-release.apk",
+			"app-hf_s36-release.apk",
+			"app-hf_s37-release.apk",
+			"app-hf_s38-release.apk",
+			"app-hf_s39-release.apk",
+			"app-hf_s40-release.apk",
+			"app-hf_s41-release.apk",
+			"app-hf_s42-release.apk",
+			"app-hf_s43-release.apk",
+			"app-hf_s44-release.apk",
+			"app-hf_s45-release.apk",
+			"app-hf_s46-release.apk",
+			"app-hf_s47-release.apk",
+			"app-hf_s48-release.apk",
+			"app-hf_s49-release.apk",
+			"app-hf_s50-release.apk",
+			"app-hf_s51-release.apk",
+			"app-hf_s52-release.apk",
+			"app-hf_s53-release.apk",
+			"app-hf_s54-release.apk",
+			"app-hf_s55-release.apk",
+			"app-hf_s56-release.apk",
+			"app-hf_s57-release.apk",
+			"app-hf_s58-release.apk",
+			"app-hf_s59-release.apk",
+			"app-hf_s60-release.apk",
+			"app-hf_s61-release.apk",
+			"app-hf_s62-release.apk",
+			"app-hf_s63-release.apk",
+			"app-hf_s64-release.apk",
+			"app-hf_s65-release.apk",
+			"app-hf_s66-release.apk",
+			"app-hf_s67-release.apk",
+			"app-hf_s68-release.apk",
+			"app-hf_s69-release.apk",
+			"app-hf_s70-release.apk",
+			"app-hf_s71-release.apk",
+			"app-hf_s72-release.apk",
+			"app-hf_s73-release.apk",
+			"app-hf_s74-release.apk",
+			"app-hf_s75-release.apk",
+			"app-hf_s76-release.apk",
+			"app-hf_s77-release.apk",
+			"app-hf_s78-release.apk",
+			"app-hf_s79-release.apk" };
 
-	static int i = 0;
+
+	static int i = 55;
 	static boolean flag = false;// 能否抓到渠道号
 	boolean apkFlag = false;// Apps中是否有对应的apk
 	static int[] badApk = new int[100];// 第一个bad的apk行号
 	static int y = 0;//// 记录第一次bad的apk
+	
+	static int sum = 0;//记录安卓6.0运行第一次，有5个权限；第二次运行就是4次权限
 
 	File classpathRoot = new File(System.getProperty("user.dir"));
 	File appDir = new File(classpathRoot, "apps");
@@ -177,7 +273,7 @@ public class Qdhtest {
 		 */
 
 		// System.out.println("apk安装启动");
-		Thread.sleep(10000);// 第一次sleep 5s,可以删除，仍然可以抓到渠道号
+		Thread.sleep(5000);// 第一次sleep 5s,可以删除，仍然可以抓到渠道号
 		testcase();
 	}
 
@@ -231,12 +327,15 @@ public class Qdhtest {
 	}
 
 	public void testcase() throws Exception {
-
+		
+		//为6.0的手机系统添加点击“始终允许”的自动点击--------------------------
+		execute6();//如果是安卓6以下，则不执行此方法
+				
 		// 执行adb
 		getDevices();
 
 		// WebDriverWait wait = new WebDriverWait(driver, 60);
-		Thread.sleep(10000);// 第二次sleep 5s，必须得有，否则抓不到
+		Thread.sleep(5000);// 第二次sleep 5s，必须得有，否则抓不到
 
 		if (flag == false) {
 			System.out.println("第" + (i + 1) + "个apk没有抓到渠道号");
@@ -259,12 +358,54 @@ public class Qdhtest {
 
 	}
 
+	private void execute6() {//安卓系统6.0的执行此方法
+
+		if(sum==0){
+			//System.out.println("sum ="+sum+","+"执行了1");
+			getPermission1();
+			
+		}else if(sum==1){
+			//System.out.println("sum ="+sum+","+"执行了2");
+			getPermission2();
+			
+		}
+	}
+
+	private void getPermission2() {
+
+		for(int i=0;i<4;i++){
+			WebElement nokjfs = driver.findElement(By.id("com.android.packageinstaller:id/permission_allow_button"));
+			//System.out.println("点击前执行第"+i+"次"+nokjfs.toString());
+			if(nokjfs.toString() != null && nokjfs.toString().length() > 0){
+				nokjfs.click();
+			}
+			
+		}
+		
+	}
+
+	private void getPermission1() {
+		
+		for(int i=0;i<5;i++){
+			WebElement nokjfs = driver.findElement(By.id("com.android.packageinstaller:id/permission_allow_button"));
+			//System.out.println("点击前执行第"+i+"次"+nokjfs.toString());
+			if(nokjfs.toString() != null && nokjfs.toString().length() > 0){
+				nokjfs.click();
+			}
+			
+		}
+		
+		sum = 1;
+		
+	}
+
 	public static String compareQDH() {
 		String content = Xlsfile.readxls("QDH", 3, (i + 1));
 		return content;
 	}
 
 	public void getDevices() {
+		
 		// 通过环境变量获取adb程序的路径
 		String adbPath = System.getenv("ANDROID_HOME") + "/platform-tools/adb.exe";
 		File file = new File(adbPath);
